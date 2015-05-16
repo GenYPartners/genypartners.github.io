615dc32cc65f23dab10b180aa0a566d11e307986
Garrett Goehring <gmgoehring@gmail.com>
2015-05-15 20:49:59 -0500

    added ability to change page

--- JS/home.js
+++ JS/home.js
@@ -1,20 +1,63 @@
-$(".logo").click(function(){
+$(".expand").click(function(){
 	var closed = $(".bar2").attr("id");
 
 	if (closed != "closed"){
-	$(".bar2").animate({"left":"0"}, "slow", function(){
+	$(".bar2").animate({"left":"0"}, "fast", function(){
 		$(this).attr("id","closed")
 	});
 
 } else{
-		$(".bar2").animate({"left":"20%"}, "slow", function(){
+		$(".bar2").animate({"left":"20%"}, "fast", function(){
 		$(this).attr("id","open")
 	});
 };
 });
 
 $(".close").click(function(){
-	$(".bar2").animate({"left":"0"}, "slow", function(){
+	$(".bar2").animate(
+		{"left":"0"}, "slow", function(){
 		$(this).attr("id","closed")
 	})
 })
+
+$(".logo").click(function(){
+	var top = $(".top").attr("id");
+	if (top != "home") {
+		console.log(top)
+		$("#"+top).animate(
+			{"z-index":"0"}, "slow"
+		);
+		$("#home").animate(
+			{"z-index":"300"}, "slow"
+		);
+		$("#home").addClass("top");
+		$(top).removeClass("top");
+	}
+})
+
+$(".why_millenials").click(function(){
+	var top = $(".top").attr("id");
+	$(".bar2").css("left","0")
+	$("#facts").animate(
+		{"z-index":"300"}, "slow"
+	);
+	$("#"+top).animate(
+		{"z-index":"0"}, "slow"
+	);
+	$("#facts").addClass("top");
+	$("#"+top).removeClass("top");
+});
+
+$(".services").click(function(){
+	var top = $(".top").attr("id");
+	$(".bar2").css("left","0")
+	$("#services").animate(
+		{"z-index":"300"}, "slow"
+	);
+	$("#"+top).animate(
+		{"z-index":"0"}, "slow"
+	);
+	$("#services").addClass("top");
+	$("#"+top).removeClass("top");
+});
+
