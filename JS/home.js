661215bd9bafb6ce72862501c85946e60cb556aa
Garrett Goehring <gmgoehring@gmail.com>
2015-05-17 15:24:16 -0500

    added content to services page

--- JS/home.js
+++ JS/home.js
@@ -37,7 +37,10 @@ $(".logo").click(function(){
 
 $(".why_millenials").click(function(){
 	var top = $(".top").attr("id");
-	$(".bar2").css("left","0")
+	$(".bar2").animate(
+		{"left":"0"}, "slow", function(){
+		$(this).attr("id","closed")
+	})
 	$("#facts").animate(
 		{"z-index":"300"}, "slow"
 	);
@@ -50,7 +53,10 @@ $(".why_millenials").click(function(){
 
 $(".services").click(function(){
 	var top = $(".top").attr("id");
-	$(".bar2").css("left","0")
+	$(".bar2").animate(
+		{"left":"0"}, "slow", function(){
+		$(this).attr("id","closed")
+	})
 	$("#services").animate(
 		{"z-index":"300"}, "slow"
 	);
@@ -63,7 +69,10 @@ $(".services").click(function(){
 
 $(".team").click(function(){
 	var top = $(".top").attr("id");
-	$(".bar2").css("left","0")
+	$(".bar2").animate(
+		{"left":"0"}, "slow", function(){
+		$(this).attr("id","closed")
+	})
 	$("#team").animate(
 		{"z-index":"300"}, "slow"
 	);
@@ -89,3 +98,48 @@ $(".contact").click(function(){
 	$("#contact").addClass("top");
 	$("#"+top).removeClass("top");
 });
+
+$(".service").click(function(){
+	var eid = $(this).attr("id");
+	var eclass = $(this).attr("class");
+	var text = "#"+eid+"-text";
+	var title = "#"+eid+"-title";
+	console.log(eid + " " + eclass);
+	if (eclass.includes("open") == false){
+	$(this).addClass("open");
+	$(text).css("display","initial");
+	$(this).css("left", "5%");
+	$(title).css("font-weight","400");
+	$(title).css("margin-left","1.5%");
+	$(title).css("top","72%")
+	if (eid == "web") {
+		$(title).css("color","white");
+		$(text).css("color","white");
+	} else if (eid == "content") {
+		$(title).css("top","50%");
+	} else if (eid == "engagement") {
+		$(title).css("color","black");
+		$(text).css("color","black");
+		$(text).css("top","55%");
+		$(title).css("top","15%");
+	};
+	}else{
+		$(this).removeClass("open");
+		$(text).css("display","none")
+		$(title).css("font-weight","100");
+		$(title).css("margin-left","0");
+		$(title).css("top","30%")
+		if (eid == "web") {
+			$(title).css("color","black");
+		};
+		if (eid == "web") {
+			$(this).css("left", "1%")
+		} else if (eid == "content") {
+			$(this).css("left", "34.33%")
+		} else if (eid == "engagement") {
+			$(this).css("left", "67.66%");
+			$(title).css("color","black");
+		};
+	}
+
+})
